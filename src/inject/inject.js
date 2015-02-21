3c049433b74cb8813112a223c0c38243aa504ca7
Will Strimling <w@ml.to>
2015-02-21 09:52:51 -0800

    upload

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
   if(isNode) {
 
-    $(".only-with-full-nav").append('<a href="#" class="minibutton sidebar-button" id="deployAzure"><span class="octicon octicon-rocket rocket"></span> Deploy to Azure</a>');
+    $(".only-with-full-nav").append('<a href="#" class="minibutton sidebar-button"><span class="octicon octicon-rocket rocket"></span> Deploy to Azure</a>');
 
     var path = $('a[aria-label^=Download]').attr('href');
-
   }
-
-
-
 });
 
 function isNode() {
