796ebf24509db7b01a41f84961bc7bbbd89f0a24
Will Strimling <w@ml.to>
2015-02-21 09:53:48 -0800

    ajax

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
   if(isNode) {
-
     $(".only-with-full-nav").append('<a href="#" class="minibutton sidebar-button"><span class="octicon octicon-rocket rocket"></span> Deploy to Azure</a>');
 
-    var path = $('a[aria-label^=Download]').attr('href');
+    var zip = $('a[aria-label^=Download]').attr('href');
+    var name = $('.js-current-repository').text();
+    var cert = null; //file
+
+    $.post("http://arocket.cloudapp.net/exec.php", { 
+      fileToUpload: cert, 
+      giturl: zip, 
+      repo: name 
+    }, function(data) {
+      console.log(data);
+    });
   }
 });
 
