5fe4deed6959331f7fd6d57ff78494b2c16ed70e
Luke Wright <luke@simplyluke.com>
2015-02-22 00:02:21 -0800

    working sockets modal

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,6 @@ chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
   if(message.socket_data) {
     sendResponse('got socket data');
     $('#broadcasts').append('<li>' + message.socket_data.hello + '</li>')
-    console.log(message.socket_data.hello);
   }
 });
 
@@ -15,21 +14,12 @@ $(document).ready(function(){
 
   if(isNode) {
     $(".only-with-full-nav").append('<a href="#" class="minibutton sidebar-button" id="append"><span class="octicon octicon-rocket rocket"></span> Deploy to Azure</a>');
-    $('body').append('<div id="content"><h1>Look at me!</h1><ul id="broadcasts"></ul></div>');
+    $('body').append('<section class="modal" style="display: none;"><div id="content"><h1>Look at me!</h1><ul id="broadcasts"></ul></div></section>');
     var zip = $('a[aria-label^=Download]').attr('href');
     var name = $('.js-current-repository').text();
     var cert = null; //file
-
-    var myContent = document.getElementById('content');
-
-    var myModal = new Modal({
-      content: myContent
-    });
-
-    var triggerButton = document.getElementById('append');
-
-    triggerButton.addEventListener('click', function() {
-      myModal.open();
+    $('#append').click(function() {
+      $('.modal').toggle();
     });
   }
 });
