344633588a9fac6c86d30a64efd0332254e2d309
Rohan Mathur <rohan@rmathur.com>
2015-02-21 21:26:07 -0600

    Fix a bug and clean up
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -9,51 +9,30 @@ chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
     $('#broadcasts').append('<li>' + message.socket_data.hello + '</li>')
     console.log(message.socket_data.hello);
   }
-}); 
+});
 
 $(document).ready(function(){
 
   if(isNode) {
     $(".only-with-full-nav").append('<a href="#" class="minibutton sidebar-button" id="append"><span class="octicon octicon-rocket rocket"></span> Deploy to Azure</a>');
-    $('body').append('<div id="content"><h1>Look at me!</h1><ul id="broadcasts"></ul></div>');
+    $('body').append('<div id="content"><h1>Look at me!</h1><ul id="broadcasts"><li>Transmitting data to our server...</li><li>Server is analyzing git repository...</li></ul></div>');
     var zip = $('a[aria-label^=Download]').attr('href');
     var name = $('.js-current-repository').text();
     var cert = null; //file
 
-    $(".rocket").click(function(e) {
-      // e.preventDefault();
-      // $.post("http://arocket.cloudapp.net/exec.php", { 
-      //   fileToUpload: cert, 
-      //   giturl: zip, 
-      //   repo: name 
-      // }, function(data) {
-      //   console.log("post reponse: " + data);
-      // });
-      // return false;
+    var myContent = document.getElementById('content');
+
+    var myModal = new Modal({
+      content: myContent
     });
 
-    window.setTimeout(function() {
-      var myContent = document.getElementById('content');
+    var triggerButton = document.getElementById('append');
 
-      var myModal = new Modal({
-        content: myContent
-      });
-
-      var triggerButton = document.getElementById('append');
-
-      triggerButton.addEventListener('click', function() {
-        myModal.open();
-      });
-    }, 3000);
+    triggerButton.addEventListener('click', function() {
+      myModal.open();
+    });
   }
-
-
-
 });
-
-
-
-
 
 function isNode() {
   return $('a[title="package.json"]').length > 0;
